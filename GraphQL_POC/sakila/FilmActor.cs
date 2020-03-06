12b0ebc7f4f01e74848d132aebb5007a8fb0a57a
Adabala Sujit <M1047107@mindtree.com>
2020-03-06 16:23:33 +0530

    added initial scaffolding for graphQL POC

---
+++ GraphQL_POC/sakila/FilmActor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphQL_POC.sakila
+{
+    public partial class FilmActor
+    {
+        public short ActorId { get; set; }
+        public short FilmId { get; set; }
+        public DateTimeOffset LastUpdate { get; set; }
+
+        public Actor Actor { get; set; }
+        public Film Film { get; set; }
+    }
+}
